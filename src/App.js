51fc22372b84c5bf61ca5cc21fe8c713918eb4d6
Minee <mingramer@gmail.com>
2021-03-28 22:31:30 +0900

    리액트를 다루는 기술(개정판)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+
 import logo from './logo.svg';
 import './App.css';
 
+/* App 이라는 컴포넌트를 만들어 준다.
+   함수에서 반환하는 내용은 HTML이 아니라 JSX이다.
+   * JSX란
+     : 자바스크립트의 확장 문법이며 XML과 매우 비슷하게 생김.
+     : 브라우저에서 실행되기 전에 코드가 번들링되는 과정에서 바벨을 사용하여 일반 자바스크립트 형태의 코드로 변환.
+*/
 function App() {
   return (
     <div className="App">
@@ -18,6 +25,7 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
