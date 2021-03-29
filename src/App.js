030ad18690146c46f57401e54017857d0025123e
Minee <mllee@ipageon.com>
2021-03-29 17:45:34 +0900

    p68 까지

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import logo from './logo.svg';
+// import './App.css';
 
 /* App 이라는 컴포넌트를 만들어 준다.
    함수에서 반환하는 내용은 HTML이 아니라 JSX이다.
@@ -9,24 +9,27 @@ import './App.css';
      : 브라우저에서 실행되기 전에 코드가 번들링되는 과정에서 바벨을 사용하여 일반 자바스크립트 형태의 코드로 변환.
 */
 function App() {
+  const name = '뤼액트'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
 
-    </div>
+    // </div>
+    // <>{name ==='리액트' && <h1>리액트입니다.</h1>}</>
+    <>{name ==='리액트' ? <h1>리액트입니다.</h1> : null}</>
   );
 }
 
