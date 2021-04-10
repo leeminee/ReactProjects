fd84cc65932b5d0ec4a0b6efb21199542786c05e
Minee <mingramer@gmail.com>
2021-04-11 01:50:58 +0900

    복습2

--- src/customAxios.js
+++ src/customAxios.js
@@ -9,9 +9,7 @@ export default function customAxios(url, callback) {
             /**
              * 개발환경에서의 크로스 도메인 이슈를 해결하기 위한 코드로
              * 운영환경에 배포할 경우에는 아래메소드 주석처리하자.
-             * 
-             * 💡 크로스 도메인 이슈: 
-             * 브라우저에서 다른 도메인으로 url 요청을 하는 경우 나타나는 보안문제
+             * 💡 크로스 도메인 이슈: 브라우저에서 다른 도메인으로 url 요청을 하는 경우 나타나는 보안문제
              */
             baseURL: 'http://localhost:8080',
             withCredentials:true,
